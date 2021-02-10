58dcd96be6dbe90114ce32091e08b5017d74becd
Alex <agamyan@outlook.com>
2021-02-10 15:08:22 +0400

    added hero section

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,25 @@
 const btnHamburger = document.querySelector('#btnHamburger');
-const header = document.querySelector('.header')
-const overlay = document.querySelector('.overlay')
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 
 btnHamburger.addEventListener('click', () => {
     if (header.classList.contains('open')) {  // Close Hamburger Menu
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out')
+        body.classList.remove('nonscroll');
+        fadeElems.forEach((element) => {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out')
+        }) 
     }
     else { // Open Hamburger Menu
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in')
+        body.classList.add('nonscroll');
+        fadeElems.forEach((element) => {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
     }
 })
 
