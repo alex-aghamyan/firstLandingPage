a2506d1be69b8161d4936bd84db039c150e3a35d
Alex <agamyan@outlook.com>
2021-02-14 14:16:58 +0400

    added form validation

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,4 @@
-const btnHamburger = document.querySelector('#btnHamburger');
+const btnHamburger = document.getElementById('btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
@@ -6,11 +6,6 @@ const body = document.querySelector('body');
 const btnRequest = document.querySelectorAll('.button');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
-const submit = document.querySelector('.submit');
-
-const fName = document.forms["popup"]["name"].value;
-const lName = document.forms["popup"]["lastname"].value;
-const phoneNum = document.forms["popup"]["tel"].value;
 
 
 btnHamburger.addEventListener('click', () => {
@@ -43,25 +38,5 @@ btnRequest.forEach((button) => {
 popupClose.addEventListener('click', () => {
     popup.style.display = "none";
     body.style.overflowY = "scroll";
+    document.popup.reset()
 });
-
-function validateForm () {
-    if (fName !== "") {
-        alert('enter only letters');
-        return false
-    }
-    if (lName != "") {
-        alert('enter only letters');
-        return false
-    }
-    if (typeof phoneNum !== 'number' ) {
-        alert('enter only numbers');
-        return false
-    }
-}
-
-submit.addEventListener('click', () => {
-    popup.style.display = "none";
-    body.style.overflowY = "scroll";
-    validateForm()
-})
